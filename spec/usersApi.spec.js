45b671387cbb072c01e29859dd334631c76acac5
Kozyrov <sfrankie11@gmail.com>
2019-03-05 08:28:28 -0600

    built out full test for POST register route

--- spec/usersApi.spec.js
+++ spec/usersApi.spec.js
@@ -1,4 +1,5 @@
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
 const baseURI = 'http://localhost:8000/api/users';
 
@@ -10,10 +11,38 @@ describe('Users API', () => {
                 'password': 'noemail'
             }
         }
-        it('should return error code 422 if no email is provided', (done) => {
+        it('should return code 422 if no email is provided', (done) => {
             return frisby.post(baseURI + '/', noEmailBody)
             .expect('status', 422)
             .done(done);
         })
     })
+    describe('POST - / - Register route', () => {
+        const noPassBody = {
+            'user': {
+                'email':'nopass@word.com',
+                'password':''
+            }
+        }
+        it('should return code 422 if no password is provided', (done) => {
+            return frisby.post(baseURI + '/', noPassBody)
+            .expect('status', 422)
+            .done(done);
+        })
+    })
+    describe('POST - / - Register route', () => {
+        const properBody = {
+            'user': {
+                'email': 'proper@body.com',
+                'password': 'success'
+            }
+        }
+        it('should return code 200 and token string when proper credentials are supplied', (done) => {
+            return frisby.post(baseURI + '/', properBody)
+            .expect('status', 200)
+            .expect('jsonTypes', 'user.token', Joi.string())
+            .done(done);
+        })
+    })
+
 });
